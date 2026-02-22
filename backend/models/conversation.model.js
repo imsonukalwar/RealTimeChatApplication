d2a59baa4fa89a3f3327c01a71af74ebf65fcf58
unknown <imsonukalwar@gmail.com>
2026-02-22 10:09:10 +0530

    All model is here like User conversesion and massage model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,19 @@
+const mongoose =require('mongoose')
+
+const  conversationSchema=new  mongoose.Schema({
+    participants:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        }
+    ],
+    messages:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"messageModel"
+        },
+    ]
+},{timestamps:true})
+
+const Conversation =mongoose.model("conversation",conversationSchema);
+module.exports=Conversation
