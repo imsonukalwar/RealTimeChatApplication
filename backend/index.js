f3dfe7af3a7ad82fa9d3e0081b5374a5a9348ecd
unknown <imsonukalwar@gmail.com>
2026-02-24 08:50:12 +0530

    final code

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const port =process.env.PORT||8000;
 
 // const app=express();
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {
+        // origin:"http://localhost:5173",
+        origin:"https://realtimechatapplication-1-qcr5.onrender.com",
     credentials:true
 }
 ))
